38e9790adf6f62e1ed92799fa27bd8e7cb485913
Falsin <greenldend@gmail.com>
2024-02-25 13:10:25 +0700

    create erb template and creating output directory with letters

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,18 +1,50 @@
 require 'csv'
-puts 'Event Manager Initialized!'
+require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+def legislators_by_zipcode(zipcode)
+  # according to https://github.com/googleapis/google-api-ruby-client/blob/main/docs/usage-guide.md
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'Event Manager Initialized!'
 
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
+
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  file_path = "output/thanks_#{id}.html"
+  File.open(file_path, 'w') do |file|
+    file.puts form_letter
+  end
 end
